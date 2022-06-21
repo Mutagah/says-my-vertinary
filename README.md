67a944eb4cf4b0676bdb63d61703329ab9bc44ad
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-21 23:33:03 +0300

    Redefined the whole layout of the webpage

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Hello,this is my first ever project on Github.
+                                        Project Name : My Veterinary Says
+
+    Written by : Kihahu Mutaga
+
+
+Project Description :
+                        My Veterinary Says is a pure web application project that seeks to slash the gap when it comes to timely response when our pets(dogs) are in need of medical attention. The web application seeks to connect clients with information on various veterinary doctors.
+
+Project Setup Instructions: 
+
+
+Link to Live Site on Github Pages:
+
+Copyright and License Information: 
--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+document.addEventListener("DOMContentLoaded",() => {
+    fetchingData()
+    //clickedFunc()
+})
 const clicked1 = document.getElementById("button1")
-clicked1.addEventListener('click',function(){
-    alert("My vet says : Humans to avoid giving their cats milk")
-});
+const ImagesDivision = document.getElementById("myImages")
+function clickedFunc(){clicked1.addEventListener("click",() => alert("Hello guys"))}
+function fetchingData(){
+fetch("https://dog.ceo/api/breed/hound/images/random/3")
+    .then(resp => resp.json())
+        .then(data =>
+            {
+                console.log(data)
+                fillingImages(data)
+            } )
+}
+function fillingImages(data){
+        const myImages = data["message"]
+        console.log(myImages)
+        myImages.forEach(element => {
+        let imgTag =  document.createElement("img")
+        imgTag.setAttribute("src",element)    
+        ImagesDivision.appendChild(imgTag)
+    });
+}
